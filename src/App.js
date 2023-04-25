4f35df899263828f94ef5a93ed38ee1a7b2b421f
unknown <swastikdubey1234@gmail.com>
2023-04-25 16:26:05 +0530

    initial start 15

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import "./App.css";
-import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/Topbar/NavBar";
 import Home from "./components/Landing-page/Home";
-
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/quickfinz">
- 
+      <HashRouter >
         <NavBar />
-        <Routes>
-          <Route path="/quickfinz" exact element={<Home />} />
-
-      
-        </Routes>
-      </BrowserRouter>
+        <Route>
+          <Route path="/"  element={<Home />} />
+        </Route>
+      </HashRouter>
     </div>
   );
 }
