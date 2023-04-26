b2b8c7075c4f3dd56414164ff31e3a364d35cc64
unknown <swastikdubey1234@gmail.com>
2023-04-26 11:50:54 +0530

    initial start 15

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import React from "react";
-import { HashRoute, Route, Routes } from "react-router-dom";
+//import { HashRoute, Route, Routes } from "react-router-dom";
 import NavBar from "./components/Topbar/NavBar";
 import Home from "./components/Landing-page/Home";
 
 function App() {
   return (
     <div className="App">
-      <HashRoute>
+
         <NavBar />
-        <Routes>
-          <Route path="/"  element={<Home />} />
-        </Routes>
-      </HashRoute>
+   
+          <Home />
+        
+   
     </div>
   );
 }
